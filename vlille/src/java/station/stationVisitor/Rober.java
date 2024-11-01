af47baa3c1839004d79a51b18cf012740ed75bc9
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-11-01 15:35:27 +0100

    Ajout des squelettes et docu

--- vlille/src/java/station/stationVisitor/Rober.java
+++ vlille/src/java/station/stationVisitor/Rober.java
@@ -1,4 +1,33 @@
 package station.stationVisitor;
 
+import station.Station;
+import vehicle.Vehicle;
+
 public class Rober implements StationVisitor {
+
+    /**
+     * Rober's constructor
+     */
+    public Rober() {
+
+    }
+
+    /**
+     * method which allows you to visit the station
+     * @param station - Station<Vehicle> The station visited
+     */
+    @Override
+    public void visit(Station<Vehicle> station) {
+
+    }
+
+    /**
+     * Steal the vehicle from the station
+     * @param station - The stolen station
+     */
+    private void robStation(Station<Vehicle> station){
+
+    }
+
+
 }
--- vlille/src/java/station/stationVisitor/StationVisitor.java
+++ vlille/src/java/station/stationVisitor/StationVisitor.java
@@ -1,4 +1,13 @@
 package station.stationVisitor;
 
+import station.Station;
+import vehicle.Vehicle;
+
 public interface StationVisitor {
+    /**
+     * method which allows you to visit the station
+     * @param station - Station<Vehicle> la station visité
+     */
+    void visit(Station<Vehicle> station);
+
 }
