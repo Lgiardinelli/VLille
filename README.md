e94abbb513182fa70246754f8c0e96e992254fee
Evan Joly <evan.joly.etu@univ-lille.fr>
2024-11-01 15:13:58 +0100

    ADD squelette sous classe VehicleCreator et DOC

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ JOLY Evan
 
 Semaine 
 Squelette de certaines classes
+
+
+### Semaine du 28/10 au 3/10
+
+- ajout squelette et doc pour les vehicles 
+- ajout squelette et doc pour les Creators 
+- ajout squelette et doc pour les VehicleVisitor
--- vlille/src/java/vehicle/vehicleVisitor/Repair.java
+++ vlille/src/java/vehicle/vehicleVisitor/Repair.java
@@ -1,4 +1,26 @@
 package vehicle.vehicleVisitor;
 
+import vehicle.Vehicle;
+
+/**
+ * class Repair
+ */
 public class Repair implements VehicleVisitor {
+
+    /**
+     * constructor
+     */
+    public Repair() {
+    }
+
+    @Override
+    public void visit(Vehicle vehicle) {
+    }
+
+    /**
+     * method that repair a vehicle who is the HS State
+      * @param vehicle the vehicle to repair
+     */
+    private void repairVehicle(Vehicle vehicle) {
+    }
 }
