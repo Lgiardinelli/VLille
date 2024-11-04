e38e159703f667d3324f22bcb70b5bb3210dd75a
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-11-04 14:46:43 +0100

    Mis en place classe abstraite Vehicle avec méthode decorate abstrait Complément de la classe de test Vehicle

--- vlille/test/java/vehicle/VehicleTest.java
+++ vlille/test/java/vehicle/VehicleTest.java
@@ -1,12 +1,29 @@
 package vehicle;
 
 import org.junit.jupiter.api.BeforeEach;
+import station.Station;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
 
+    Station<Bike> station;
+    Bike bike;
+
     @BeforeEach
     void setUp() {
+         station = new Station();
+         bike = new Bike();
+         station.addVehicle(bike);
     }
+
+
+
+
+
+
+
+
+
+
 }
