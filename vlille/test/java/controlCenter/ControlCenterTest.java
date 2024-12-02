ee8d9e30fc30c13dae0bd04f034d08f7cbc6a4ef
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-12-02 13:23:17 +0100

    Ajout tests

--- vlille/test/java/controlCenter/ControlCenterTest.java
+++ vlille/test/java/controlCenter/ControlCenterTest.java
@@ -1,12 +1,38 @@
 package controlCenter;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import station.Station;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControlCenterTest {
 
+    Station station;
+    ControlCenter controlCenter;
+
     @BeforeEach
     void setUp() {
+        controlCenter = new ControlCenter();
+        station = new Station();
+    }
+
+    @Test
+    void notifyStationVehicleAddedTest() {
+        assertTrue(controlCenter.getStations().isEmpty());
+        station.addSubscriber(controlCenter);
+        controlCenter.addStationToListControlCenter(station);
+        controlCenter.notifyStationVehicleAdded(station);
+        assertFalse(controlCenter.getStations().isEmpty());
+        assertEquals(0, controlCenter.getNbVehicleStation(station));
+
+    }
+
+    @Test
+    void notifyStationVehicleTakedTest() {
+        station.addSubscriber(controlCenter);
+        controlCenter.addStationToListControlCenter(station);
+        controlCenter.notifyStationVehicleAdded(station);
+
     }
 }
