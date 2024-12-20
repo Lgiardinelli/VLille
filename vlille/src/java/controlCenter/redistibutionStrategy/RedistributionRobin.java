ca8f688a6fa7db68e1e9db6f59534ad5a8211b38
Léo <leo.giardinelli.etu@univ-lille.fr>
2024-12-20 15:24:21 +0100

    Ecriture de la classe RedistributionRobin

--- vlille/src/java/controlCenter/redistibutionStrategy/RedistributionRobin.java
+++ vlille/src/java/controlCenter/redistibutionStrategy/RedistributionRobin.java
@@ -1,10 +1,60 @@
 package controlCenter.redistibutionStrategy;
 
+import exeption.NoVehicleOfThisTypeAvailableException;
+import exeption.StationEmptyException;
+import exeption.StationFullException;
+import station.Station;
+import vehicle.Vehicle;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * RedistributionRobin class
  */
 public class RedistributionRobin implements RedistributionStrategy{
 
+    Random random = new Random();
+
     @Override
-    public void reallocation() {}
+    public void reallocation(Set<Station> stations, Station station) throws StationFullException, StationEmptyException, NoVehicleOfThisTypeAvailableException {
+        if (!station.canBeDropOff())
+            reallocationRobin(stations, station, true);
+        else if (!station.canBeRent())
+            reallocationRobin(stations, station, false);
+    }
+
+    private void reallocationRobin(Set<Station> stations, Station station, boolean isFull) throws StationFullException, StationEmptyException, NoVehicleOfThisTypeAvailableException {
+        int nbReallocation = station.getCapacityMax()/2;
+        if (isFull) {
+            for (int i=0; i<nbReallocation; i++) {
+                List<Station> filterStation = stations.stream().filter(s -> s.canBeDropOff()).collect(Collectors.toList());
+                if (!filterStation.isEmpty()) {
+                    Vehicle vehicle = station.rentVehicle(v -> v instanceof Vehicle);
+                    Station s = takeEmptiestStation(filterStation);
+                    s.dropOffVehicle(vehicle);
+                }
+            }
+        }
+        else {
+            for (int i=0; i<nbReallocation/2;i++) {
+                List<Station> filterStation = stations.stream().filter(s -> (s.canBeRent())).collect(Collectors.toList());
+                if (!filterStation.isEmpty()) {
+                    Station s = takeFullestStation(filterStation);
+                    Vehicle vehicle = s.rentVehicle(v -> v instanceof Vehicle);
+                    station.dropOffVehicle(vehicle);
+                }
+            }
+        }
+    }
+
+    private Station takeFullestStation(List<Station> stations) {
+        Optional<Station> stationFullest = stations.stream().max(Comparator.comparingInt(s -> s.getVehicles().size()));
+        return stationFullest.orElse(null);
+    }
+
+    private Station takeEmptiestStation(List<Station> stations) {
+        Optional<Station> stationFullest = stations.stream().min(Comparator.comparingInt(s -> s.getVehicles().size()));
+        return stationFullest.orElse(null);
+    }
 }
