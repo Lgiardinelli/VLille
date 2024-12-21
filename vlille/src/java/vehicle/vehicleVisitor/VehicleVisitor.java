e1e5f181813a100c8d73f9b154bac67de133920e
Léo <leo.giardinelli.etu@univ-lille.fr>
2024-12-21 21:40:38 +0100

    Ajout méthode dans l'interface VehicleVisitor

--- vlille/src/java/vehicle/vehicleVisitor/VehicleVisitor.java
+++ vlille/src/java/vehicle/vehicleVisitor/VehicleVisitor.java
@@ -2,6 +2,8 @@ package vehicle.vehicleVisitor;
 
 import vehicle.Vehicle;
 
+import java.util.List;
+
 /**
  * interface VehicleVisitor
  */
@@ -10,5 +12,20 @@ public interface VehicleVisitor {
      * method that visit a vehicle and do something on it
      * @param vehicle a vehicle to visit
      */
-    public void visit(Vehicle vehicle);
+    public void visit(Vehicle vehicle) throws Exception;
+
+    /**
+     * method that visit a vehicle and do something on it
+     * @param vehicle a vehicle to visit
+     * @return boolean that represent if the vehicle is accepted or not
+     */
+    public boolean filterCondition(Vehicle vehicle);
+
+    /**
+     * method affichage
+     * @return String that represent the object
+     */
+    public String toString();
+
+    public void timerControl();
 }
