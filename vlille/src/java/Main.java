94ae6f11a76647aac395c8f35eae128fe2084d04
Léo <leo.giardinelli.etu@univ-lille.fr>
2024-12-22 18:24:19 +0100

    Update main

--- vlille/src/java/Main.java
+++ vlille/src/java/Main.java
@@ -1,7 +1,13 @@
 import controlCenter.ControlCenter;
 import displayer.ConsoleDisplayer;
 import displayer.DisplayerInterface;
+import exeption.NoVehicleOfThisTypeAvailableException;
+import exeption.StationEmptyException;
 import exeption.StationFullException;
+import listchooser.ListChooser;
+import listchooser.RandomListChooser;
+import listchooser.RandomSetChooser;
+import listchooser.SetChooser;
 import station.Station;
 import station.stationVisitor.Rober;
 import station.stationVisitor.StationVisitor;
@@ -26,15 +32,36 @@ public class Main {
     private static final StationVisitor rober = new Rober();
     private static final List<VehicleVisitor> workerVec = new ArrayList<>();
     private static final List<Vehicle> rentedVec = new ArrayList<>();
+    private static ListChooser<Vehicle> listChooserVehicle = new RandomListChooser<>();
+    private static SetChooser<Vehicle> vehicleSetChooser = new RandomSetChooser<>();
+    private static SetChooser<Station> stationSetChooser = new RandomSetChooser<>();
+    private static Random random = new Random();
 
-    private static void roundDropOff(){
+    private static void roundDropOff() throws StationFullException {
         if(!rentedVec.isEmpty()){
-
+            int nbDropOff = random.nextInt(1,rentedVec.size()+1);
+            for (int i=0; i<nbDropOff; i++) {
+                dropVehicleStationAlea();
+            }
+        }
+        else {
+            System.out.println("Aucun véhicule n'a été loué !");
         }
     }
 
-    private static void roundRent(){
+    private static void dropVehicleStationAlea() throws StationFullException {
+        Vehicle vehicle = listChooserVehicle.choose(rentedVec);
+        rentedVec.remove(vehicle);
+        Station station = stationSetChooser.choose(mainControlCenter.getStations().keySet());
+        station.dropOffVehicle(vehicle);
+        System.out.println("Le véhicule " + vehicle.getClass() + " a été déposé à la station " + station.getId());
+    }
 
+    private static void roundRent() throws StationEmptyException, NoVehicleOfThisTypeAvailableException {
+        Station station = stationSetChooser.choose(mainControlCenter.getStations().keySet());
+        Vehicle vehicle = station.rentVehicle(Vehicle.class::isInstance);
+        rentedVec.add(vehicle);
+        System.out.println("Le vehicule " + vehicle.getClass() + " a été loué dans la station " + station.getId());
     }
 
     private static void roundUpdate(){
@@ -43,7 +70,8 @@ public class Main {
 
 
 
-    public static void main(String[] arg) throws StationFullException {
+
+    public static void main(String[] arg) throws StationFullException, StationEmptyException, NoVehicleOfThisTypeAvailableException {
 
 
 
@@ -83,16 +111,20 @@ public class Main {
         }
         allVec.iterator().next().toHS();
         DisplayerInterface t = new ConsoleDisplayer();
-        t.displayControlCenter(mainControlCenter);
+//        t.displayControlCenter(mainControlCenter);
+//
+//        t.displayStation(allStation.get(2));
 
-        t.displayStation(allStation.get(2));
-
-//        int nb_tour = 0;
-//        while(nb_tour < 20){
-//            roundDropOff();
-//            roundRent();
-//            roundUpdate();
-//        }
+        int nb_tour = 0;
+        while(nb_tour < 2){
+            System.out.println("Tour n°" + nb_tour);
+            t.displayControlCenter(mainControlCenter);
+            roundDropOff();
+            roundRent();
+            roundUpdate();
+            t.displayControlCenter(mainControlCenter);
+            nb_tour++;
+        }
 
 
 
