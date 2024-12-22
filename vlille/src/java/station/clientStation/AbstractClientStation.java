989d20c5cee398d863b1f3137fbd6d580537694d
Léo <leo.giardinelli.etu@univ-lille.fr>
2024-12-22 15:17:05 +0100

    Changement du retour de la methode visit

--- vlille/src/java/station/clientStation/AbstractClientStation.java
+++ vlille/src/java/station/clientStation/AbstractClientStation.java
@@ -3,6 +3,7 @@ package station.clientStation;
 import exeption.NoVehicleOfThisTypeAvailableException;
 import exeption.StationEmptyException;
 import station.Station;
+import vehicle.Vehicle;
 
 public abstract class AbstractClientStation {
 
@@ -10,14 +11,15 @@ public abstract class AbstractClientStation {
      * visit a station a take a vehicle of the type wanted
      * @param station the station selected to take a vehicle
      */
-    public void visit(Station station) {
+    public Vehicle visit(Station station) {
         try {
-            station.rentVehicle(this.testMethod());
+            return station.rentVehicle(this.testMethod());
         } catch (NoVehicleOfThisTypeAvailableException e) {
             System.out.println(e);
         } catch (StationEmptyException e) {
-            throw new RuntimeException(e);
+            System.out.println(e);
         }
+        return null;
     }
 
     /**
